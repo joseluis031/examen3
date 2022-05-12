35ffbe181973e0b924cda6f83d9624bdf1f1bf6b
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-05-12 09:46:03 +0200

    transformacion calls

--- Clases/ejercicio.py
+++ Clases/ejercicio.py
@@ -1,13 +1,27 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import os 
+import sys
+import csv
 
 
 class Ejercicio:
     def __init__(self,datos):
         self.datos = pd.read_csv(datos)
-    def cotar_usuarios(self):
-        self.datos["uuid"].count()
+        self.datos.dropna()
+    def cotar_usuarios(self): #con esta funcion cuento cuanta gente entra en a web
+        contar = self.datos["uuid"].count()
+        return contar
+    def transformaciones(self): #con esta funcion se cuenta cuantos clientes finalmente han realizado la conversion con un bucle
+        contador = 0  
+        for i in self.datos["lead_type"]:
+            if i == "CALL":
+                contador = contador + 1
+            elif i =="FORM":
+                pass
+        return contador
         
-hola = Ejercicio("navegacion (4).csv")
+hola = Ejercicio("conversiones (4).csv")
 hola.cotar_usuarios()
+
