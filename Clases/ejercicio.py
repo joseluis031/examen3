7fe1dc9967b101a9a22f132f2be6d6f94c14aea9
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-05-12 09:53:20 +0200

    recurrencia

--- Clases/ejercicio.py
+++ Clases/ejercicio.py
@@ -22,9 +22,19 @@ class Ejercicio:
             elif i =="FORM":
                 contador_form = contador_form + 1
         return contador_call, contador_form
+    def recurrentes(self):
+        contador_recurrente = 0
+        contador_no_recur = 0
+        for i in self.datos["user_recurrent"]:
+            if i == "True":
+                contador_recurrente = contador_recurrente + 1
+            elif i == "False":
+                contador_no_recur = contador_no_recur + 1
+        return contador_recurrente, contador_no_recur
         
 hola = Ejercicio("conversiones (4).csv")
 hola2 = Ejercicio("navegacion (4).csv")
 hola2.cotar_usuarios()
 hola.transformaciones()
+hola2.recurrentes()
 
