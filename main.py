efd4b5b99d590555f2f9da2346ee8a885fe1beff
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-05-12 10:27:55 +0200

    mas cambios

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ if __name__ == "__main__":
     hola2.recurrentes()
     hola2.repetidos()
     hola2.coche_mas_visto()
+    
+    
+    from Clases.graficass import *
+    
+    hola2 = Graficas("navegacion (4).csv")
+    hola3 = Graficas("conversiones (4).csv")
+    print(hola2.histograma_url_landing())
+    print(hola3.histograma_user_recurrent())
+    print(hola3.histograma_lead_type())
